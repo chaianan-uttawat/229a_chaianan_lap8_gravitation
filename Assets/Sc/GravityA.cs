4fe06d2b77b106202be273238401510377684bb0
chaianan <chaianan.utta@bumail.net>
2025-03-10 10:07:04 +0700

    CodeGravity

---
+++ Assets/Sc/GravityA.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+public class GravityA : MonoBehaviour
+{
+    Rigidbody rb;
+    private const float G = 0.00674f;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        //aa
+    }
+
+    void Attract(GravityA other)
+    {
+        Rigidbody rbOther = GetComponent<Rigidbody>();
+
+        Vector3 direction = rb.position - other.rb.position;
+
+        float distance = direction.magnitude;
+
+        if (distance == 0) { return;}
+        
+        float forceMagnitude = G * (rb.mass * other.rb.mass)/Mathf.Pow(distance,2);
+        Vector3 force = forceMagnitude * direction.normalized;
+    }
+}
