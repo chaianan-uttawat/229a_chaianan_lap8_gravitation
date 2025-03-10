3e627c4d80d332d79f18f392522496a98dd39488
chaianan <chaianan.utta@bumail.net>
2025-03-10 10:49:11 +0700

    finish

--- Assets/Sc/GravityA.cs
+++ Assets/Sc/GravityA.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GravityA : MonoBehaviour
@@ -6,28 +7,40 @@ public class GravityA : MonoBehaviour
     Rigidbody rb;
     private const float G = 0.00674f;
 
-
+    public static List<GravityA> gravityObjectList;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+
+        if (gravityObjectList == null)
+        {
+            gravityObjectList = new List<GravityA>();
+        }
+        
+        gravityObjectList.Add(this);
     }
 
     private void FixedUpdate()
     {
-        //aa
+        //all Attract
+        foreach (GravityA obj in gravityObjectList)
+        {
+            Attract(obj);
+        }
     }
 
     void Attract(GravityA other)
     {
-        Rigidbody rbOther = GetComponent<Rigidbody>();
+        Rigidbody rbOther = other.rb;
 
-        Vector3 direction = rb.position - other.rb.position;
+        Vector3 direction = rb.position - rbOther.position;
 
         float distance = direction.magnitude;
 
         if (distance == 0) { return;}
         
-        float forceMagnitude = G * (rb.mass * other.rb.mass)/Mathf.Pow(distance,2);
+        float forceMagnitude = G * (rb.mass * rbOther.mass)/Mathf.Pow(distance,2);
         Vector3 force = forceMagnitude * direction.normalized;
+        rbOther.AddForce(force);
     }
 }
